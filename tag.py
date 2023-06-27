4cdc21db970947916b66a58d0ac71c8a0279045a
Kiskadee <Kiskadee.dev@gmail.com>
2023-06-27 11:09:11 -0300

    Refactor to POO

--- tag.py
+++ tag.py
@@ -26,18 +26,6 @@ from rich import print
 from rich.progress import track
 from typing import Tuple
 
-parser = argparse.ArgumentParser()
-parser.add_argument("path")
-args = parser.parse_args()
-
-target_dir = Path(args.path)
-
-if not target_dir.exists():
-    print(f"[red]Target path does not exist! {target_dir}[/red]")
-    raise FileNotFoundError("Path does not exists")
-
-geolocator = Nominatim(user_agent="geoapiExercises")
-
 
 def get_exif_data(image) -> dict[str, dict]:
     """
@@ -87,57 +75,81 @@ def dms_to_dd(exif: dict) -> Tuple[float, float]:
     return get_dd(dms_lat), get_dd(dms_long)
 
 
-paths_to_process: list[Path] = []
+class TAGGER:
+    GEOLOCATOR = Nominatim(user_agent="geoapiExercises")
 
+    def __init__(self):
+        self.paths_to_process: list[Path] = []
 
-def recurse(folder) -> None:
-    """
-    Find the files and add jpeg images to queue recursively
-    :param folder: path to the folder
-    :return: None
-    """
-    files = os.listdir(folder)
-    for f in files:
-        path = Path(folder, f)
-        if path.is_dir():
-            recurse(path)
-        elif path.is_file():
-            if not Path(str(path) + ".txt").exists() and (
-                path.name.endswith(".jpg") or path.name.endswith(".jpeg")
-            ):
-                print(f"[yellow]Queued {path}[/yellow]")
-                paths_to_process.append(path)
-            else:
-                print(f"[cyan]Skipping file[/cyan]")
+    def recurse(self, folder: Path) -> None:
+        """
+        Find the files and add jpeg images to queue recursively
+        :param folder:
+        path to images folder
 
-
-def write_tags(paths: list[Path]):
-    """
-    Write tags to a text file next to the image based on gps coordinates
-    :param paths: Queue of jpeg paths to process
-    :return: None
-    """
-
-    for file_path in track(paths, description="Processing images.."):
-        try:
-            with Image.open(file_path) as image:
-                print(f"Opening [blue]{file_path}[/blue]")
-                exif = get_exif_data(image)
-                if file_path.name.endswith(".tiff"):
-                    print(f"[gray]Tiff is not supported yet[/gray]")
-                elif exif is None or "GPSInfo" not in exif.keys():
-                    print(f"[blink red]No gps data in exif[/blink red]")
+        :return:
+        None
+        """
+        files = os.listdir(folder)
+        for f in files:
+            path = Path(folder, f)
+            if path.is_dir():
+                self.recurse(path)
+            elif path.is_file():
+                if not Path(str(path) + ".txt").exists() and (
+                    path.name.endswith(".jpg") or path.name.endswith(".jpeg")
+                ):
+                    print(f"[yellow]Queued: {path}[/yellow]")
+                    self.paths_to_process.append(path)
                 else:
-                    lat, long = dms_to_dd(get_exif_data(image))
-                    location = geolocator.reverse(str(lat) + "," + str(long))
-                    print(f"[green]{location}[/green]")
-                    with open(str(file_path) + ".txt", "w", encoding="utf-8") as tagfile:
-                        tagfile.write(f"{location}")
-                        tagfile.close()
+                    print(f"[cyan]Skipping: {path}[/cyan]")
 
-        except OSError:
-            print(f"[red]Can't open {file_path}[/red]")
+    def write_tags(self):
+        """
+        Write tags to a text file next to the image based on gps coordinates
+
+        This function is slow and requires internet access
 
 
-recurse(target_dir)
-write_tags(paths_to_process)
+        :param paths: Queue of jpeg paths to process
+        :return: None
+        """
+        paths: list[Path] = self.paths_to_process
+
+        for file_path in track(paths, description="Processing images.."):
+            try:
+                with Image.open(file_path) as image:
+                    print(f"Opening: [blue]{file_path}[/blue]")
+                    exif = get_exif_data(image)
+                    if file_path.name.endswith(".tiff"):
+                        print(f"[gray]Tiff is not supported yet[/gray]")
+                    elif exif is None or "GPSInfo" not in exif.keys():
+                        print(f"[blink red]No gps data in exif[/blink red]")
+                    else:
+                        lat, long = dms_to_dd(get_exif_data(image))
+                        location = self.GEOLOCATOR.reverse(str(lat) + "," + str(long))
+                        print(f"[green]{location}[/green]")
+                        with open(
+                            str(file_path) + ".txt", "w", encoding="utf-8"
+                        ) as tagfile:
+                            tagfile.write(f"{location}")
+                            tagfile.close()
+
+            except OSError:
+                print(f"[red]Can't open {file_path}[/red]")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    args = parser.parse_args()
+
+    target_dir = Path(args.path)
+
+    if not target_dir.exists():
+        print(f"[red]Target path does not exist! {target_dir}[/red]")
+        raise FileNotFoundError("Path does not exists")
+
+    tagger = TAGGER()
+    tagger.recurse(target_dir)
+    tagger.write_tags()
